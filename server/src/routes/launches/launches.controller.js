26f34fa97c1f49dc4298ebfe10b9eea2a7274e87
Asif-28 <asif.a.7277@gmail.com>
2023-09-14 01:24:33 +0530

    functionality to create new launch and delete it

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,13 +1,14 @@
 const {
   getAllLaunches,
-  addNewLaunch,
   existsLaunchWithId,
   abortLaunchById,
+  scheduleNewLaunch,
 } = require("../../models/launches.model");
-function httpGetAllLaunches(req, res) {
-  return res.status(200).json(getAllLaunches());
+
+async function httpGetAllLaunches(req, res) {
+  return res.status(200).json(await getAllLaunches());
 }
-function httpAddNewLaunch(req, res) {
+async function httpAddNewLaunch(req, res) {
   const launch = req.body;
   if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.target)
     return res.status(400).json({
@@ -19,17 +20,23 @@ function httpAddNewLaunch(req, res) {
       error: "Invalid launch date",
     });
   }
-  addNewLaunch(launch);
+  await scheduleNewLaunch(launch);
   return res.status(201).json(launch);
 }
-function httpAbortLaunch(req, res) {
+async function httpAbortLaunch(req, res) {
   const launchId = Number(req.params.id);
-  if (!existsLaunchWithId(launchId)) {
+  const existsLaunch =await existsLaunchWithId(launchId);
+  if (!existsLaunch) {
     return res.status(404).json({
       error: "launch not found",
     });
   }
-  const aborted = abortLaunchById(launchId);
+  const aborted = await abortLaunchById(launchId);
+  if(!aborted){
+    return res.status(400).json({
+      error:'Launch not aborted',
+    })
+  }
   return res.status(200).json(aborted);
 }
 module.exports = { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch };
